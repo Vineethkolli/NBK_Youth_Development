e89574e90988b450dbf9afe4fd1d73058efbd57e
Vineethkolli <kz8413@srmist.edu.in>
2025-02-11 20:07:09 +0530

    notifications updated

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,18 +1,16 @@
-// sw.js in your src directory
+import { precacheAndRoute } from 'workbox-precaching';
 
-// Precache manifest will be injected here by VitePWA
-self.__WB_MANIFEST;
+// This line tells Workbox where to inject the manifest.
+precacheAndRoute(self.__WB_MANIFEST);
 
 // Your push notification listener
 self.addEventListener('push', (event) => {
   const data = event.data.json();
-
   const options = {
     body: data.body,
-    icon: './logo.png', // Ensure path is correct relative to service worker
+    icon: './logo.png',
     badge: './logo.png',
   };
-
   event.waitUntil(
     self.registration.showNotification(data.title, options)
   );
